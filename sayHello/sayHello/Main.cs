aad432e3f397bb550b3004179ac2e784ab9dfddc
lai <779651003@qq.com>
2016-04-16 18:55:39 +0800

    add a c# project

---
+++ sayHello/sayHello/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace sayHello
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}
