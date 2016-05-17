1dfad6dc48f46d8f75ee528dddab5f2152231e06
xianglai <xianglai.meng@imgchina.com.cn>
2016-05-17 18:09:04 +0800

    增加个类

--- sayHello/sayHello/Main.cs
+++ sayHello/sayHello/Main.cs
@@ -9,9 +9,18 @@ namespace sayHello
 			Console.WriteLine ("Hello World!");
 			Console.WriteLine("input a word,please!");
 			string s = Console.ReadLine().ToString();
-			if ( s!= null) {
+			if ( s=="lai") {
 				Console.WriteLine("good!");
 				Console.WriteLine("thank you");
+				Man my = new Man();
+				my.Weight = 70;
+				my.Age = 30;
+				Console.WriteLine("this is my information");
+				Console.WriteLine("age:"+my.Age.ToString()+" weight:"+my.Weight.ToString());
+			}
+			else
+			{
+				Console.WriteLine("a bad man!");
 			}
 			Console.ReadLine();
 		}
---
+++ sayHello/sayHello/Man.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: XIANGLAI
+ * Date: 2016-5-17
+ * Time: 17:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace sayHello
+{
+	/// <summary>
+	/// Description of Man.
+	/// </summary>
+	public class Man
+	{
+		public Man()
+		{
+		}
+			private float weight;
+			private int age;
+			public float Weight {get;set;}
+			public int Age {get;set;}
+	}
+}
